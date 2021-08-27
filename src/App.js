b09dc76f11be52d307d2f328d6011bb125d50206
roybrushett <roy.brushett@keyin.com>
2021-08-27 15:19:15 -0230

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route,
+  Link
+} from 'react-router-dom';
+import { useRef, useState } from 'react';
+
+function NumberDisplayPage(props){
+  return (<div className="number-output-container">
+    <p>The numbers are: </p><output>{JSON.stringify(props.numbers)}</output>
+  </div>)
+}
+
+function NumberInputPage(props){
+  const inputRef = useRef()
+  const addNumber = (e)=>{
+    if(inputRef.current){
+      let newNumbers = [...props.numbers];
+      let inputNumber = parseFloat(inputRef.current.value) 
+      
+      newNumbers.push(inputNumber);
+      props.setNumbers(newNumbers);
+    }
+  }
+
+  return (<form className="number-input-form">
+      <label htmlFor="numbers">Input a number:</label>
+      <input ref={inputRef} name="numbers" type="text" placeholder="123" />
+      <button type="button" onClick={addNumber}>Submit Number!</button>
+    </form>)
+}
+
+function NavBar(){
+  return (<nav className="main-navigation">
+    <Link to="/">Input Numbers</Link>
+    <Link to="/display">Display Numbers</Link>
+  </nav>)
+}
+
+function App() {
+  let initialState = []
+  if(localStorage.getItem('scores') !== null){
+    let currentScores = localStorage.getItem('scores');
+    initialState = JSON.parse(currentScores);
+  }
+
+  let [numbers, setNumbers] = useState(initialState);
+
+  const modifiedSetNumbers = (newNumbers)=>{
+    setNumbers(newNumbers);
+    localStorage.setItem('scores', JSON.stringify(newNumbers));
+  }
+
+  return (
+    <Router>
+      <NavBar></NavBar>
+      <Switch>
+        <Route exact path="/" render={()=><NumberInputPage numbers={numbers} setNumbers={modifiedSetNumbers}></NumberInputPage>}></Route>
+        <Route exact path="/display" render={()=><NumberDisplayPage numbers={numbers}></NumberDisplayPage>}></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
